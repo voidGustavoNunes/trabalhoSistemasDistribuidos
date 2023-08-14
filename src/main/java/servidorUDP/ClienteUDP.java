40b910686655de2525c4ab194e3c223c1a82b9cf
gustavo.nunes <gustavonunes392@gmail.com>
2023-08-14 16:32:38 -0300

    atualizacao

--- src/main/java/servidorUDP/ClienteUDP.java
+++ src/main/java/servidorUDP/ClienteUDP.java
@@ -4,10 +4,60 @@
  */
 package servidorUDP;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author Gustavo
  */
 public class ClienteUDP {
+    private String nomeDNS;
+    private int serverPort;
+    private byte[] meuIP;
+    
+    
+    public ClienteUDP(){
+        try{
+
+            InetAddress endereco = InetAddress.getLocalHost();
+
+            nomeDNS = endereco.getHostName();
+
+            meuIP = endereco.getAddress();
+
+
+        }catch (UnknownHostException e){
+            System.out.println("Host Desconhecido: "+e.getMessage( ));
+        }
+        serverPort = 6789;
+    }
+    
+    
+    public ClienteUDP(String nomeDNSServidor){
+        //Metodo construtor para quando SERVIDOR e CLIENTE rodarem em
+        //maquinas diferentes
+    
+        nomeDNS = "SIGEX-B06"; //Introduza o nome do computador em que seu 
+                                //servidor for executado
+        
+        meuIP = null;
+        serverPort = 6789;
+    }
+    
+    public String getNomeDNS(){
+        return nomeDNS;
+    
+    }
+    public int getServerPort(){
+        return serverPort;
+    
+    }
+    public String getMeuIP(){
+        String s = new String(meuIP);
+        return s;
+    
+    }
+    
     
 }
